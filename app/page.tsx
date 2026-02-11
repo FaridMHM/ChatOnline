2ab810cae091f372cf7b08c1879f7d271e0c47bc
FMHM <faridmhm77@gmail.com>
2026-02-11 10:34:58 +0330

    feat: add home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
 "use client";
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <div className="w-full h-screen p-10  bg-[#dededede] flex items-center justify-center">
+      <main className=" w-full sm:w-[60%] xl:w-[60%]  md:w-[60%]  h-dvh flex flex-col items-center justify-evenly">
+        <div className="w-full  flex flex-col gap-2 items-center justify-between [&>button]:cursor-pointer  [&>button]:hover:bg-white/40 [&>button]:active:bg-white/40 [&>button]:hover:text-black [&>button]:active:text-black">
+          <button className="w-50 transition-all bg-black text-white h-12 rounded-xl">
+            Sgin Up
+          </button>
+          <button className="w-50 transition-all bg-black text-white h-12 rounded-xl">
+            Login
+          </button>
+        </div>
+        <div className="w-full h-80 p-2 ">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          mollitia magnam tempore iure? Veniam delectus, autem maxime eos
+          deleniti eligendi velit, ab ratione accusamus, iure reprehenderit
+          quisquam veritatis quo? Nam.
+        </div>
+      </main>
+    </div>
+  );
+}
