a3c4ca26edba99f2c8fb4d8ca80e29bbab36acc4
FMHM <faridmhm77@gmail.com>
2026-02-12 21:41:32 +0330

    feat: create home page and Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Header from "@/components/header/header";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen p-10  bg-[#dededede] flex items-center justify-center">
-      <main className=" w-full sm:w-[60%] xl:w-[60%]  md:w-[60%]  h-dvh flex flex-col items-center justify-evenly">
-        <div className="w-full  flex flex-col gap-2 items-center justify-between [&>button]:cursor-pointer  [&>button]:hover:bg-white/40 [&>button]:active:bg-white/40 [&>button]:hover:text-black [&>button]:active:text-black">
+    <div className="   w-full h-screen  bg-[#181c31] flex items-center justify-center">
+      <main className="  w-full max-w-7xl  h-dvh flex flex-col items-center justify-evenly">
+        <Header />
+        <div className="w-full  flex flex-col gap-2 items-center justify-between [&>button]:cursor-pointer  [&>button]:hover:bg-[#2d2f40] [&>button]:active:bg-white/40 [&>button]:hover:text-white [&>button]:active:text-black">
           <button className="w-50 transition-all bg-black text-white h-12 rounded-xl">
             Sgin Up
           </button>
@@ -12,7 +15,7 @@ export default function Home() {
             Login
           </button>
         </div>
-        <div className="w-full h-80 p-2 ">
+        <div className="w-full h-80 p-2 text-white ">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
           mollitia magnam tempore iure? Veniam delectus, autem maxime eos
           deleniti eligendi velit, ab ratione accusamus, iure reprehenderit
